5c22ee106f240a4d18c99df233c714ad27897d8a
fariez11 <fariezilham03@gmail.com>
2023-03-07 14:33:57 +0700

    buat migrasi,route dan controller

---
+++ database/migrations/2023_03_07_065845_create_katalog_mobil.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('katalog_mobil', function (Blueprint $table) {
+            $table->id();
+            $table->string('brand',50);
+            $table->string('type',50);
+            $table->integer('tahun');
+            $table->integer('harga');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('katalog_mobil');
+    }
+};
