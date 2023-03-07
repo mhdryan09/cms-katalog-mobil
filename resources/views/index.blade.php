a8a106d8da4343ade06eea8f357037f7ae64b039
fariez11 <fariezilham03@gmail.com>
2023-03-07 16:22:04 +0700

    view terbaru

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,26 +1,29 @@
 @extends('layout.main')
 
 @section('container')
-<table class="table">
-    <thead>
-      <tr>
-        <th scope="col">#</th>
-        <th scope="col">Brand</th>
-        <th scope="col">Type</th>
-        <th scope="col">Tahun</th>
-        <th scope="col">Harga</th>
-      </tr>
-    </thead>
-    <tbody>
-        @foreach ($data as $data )
+<main class="container">
+
+    <table class="table">
+        <thead>
         <tr>
-            <th scope="row">{{ $data->id }}</th>
-            <td>{{ $data->brand }}</td>
-            <td>{{ $data->type }}</td>
-            <td>{{ $data->tahun }}</td>
-            <td>{{ $data->harga }}</td>
+            <th scope="col">#</th>
+            <th scope="col">Brand</th>
+            <th scope="col">Type</th>
+            <th scope="col">Tahun</th>
+            <th scope="col">Harga</th>
         </tr>
-        @endforeach
-    </tbody>
-  </table>
+        </thead>
+        <tbody>
+            @foreach ($data as $data )
+            <tr>
+                <th scope="row">{{ $data->id }}</th>
+                <td>{{ $data->brand }}</td>
+                <td>{{ $data->type }}</td>
+                <td>{{ $data->tahun }}</td>
+                <td>{{ $data->harga }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</main>
 @endsection
